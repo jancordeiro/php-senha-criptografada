31965224768d2b0f6f2d1d0b1db16d0f8cd06e4d
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2024-04-19 10:40:56 -0300

    Update register.php

--- register.php
+++ register.php
@@ -1,15 +1,13 @@
 <?php
 
+// recebe o valor do campo de name "passwd" e criptografa
+// a criptografia se dá pela função "password_hash"
+// com o parâmetro "PASSWORD_DEFAULT"
+include('conexao.php');
+
 if(isset ($_POST['email'])) {
 	
-	include('conexao.php');
-	
-	$email = $_POST['email'];
-	
-	// recebe o valor do campo de name "passwd" e criptografa
-	// a criptografia se dá pela função "password_hash"
-	// com o parâmetro "PASSWORD_DEFAULT"
-	
+	$email = $_POST['email'];	
 	$senha = password_hash($_POST['passwd'], PASSWORD_DEFAULT);
 	
 	$mysqli->query("INSERT INTO senhas (email, senha) VALUES ('$email', '$senha')");
